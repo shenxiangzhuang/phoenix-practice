7547ff11afcad6b7d0a5f18817772b6ffc6c09b1
Mathew Shen <datahonor@gmail.com>
2024-01-31 12:35:34 +0800

    add: plugs (#3)

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
