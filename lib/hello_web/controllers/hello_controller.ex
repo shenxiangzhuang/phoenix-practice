329ecf1200d39dc5ea84cbf9ce147d903f520ef1
Xiangzhuang Shen <datahonor@gmail.com>
2024-02-03 15:01:00 +0800

    add: heex content

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -2,7 +2,12 @@ defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
+    # conn
+    # |> put_root_layout(html: :app)
+    # |> render(:index)
+
     render(conn, :index)
+
   end
 
   def show(conn, %{"messenger" => messenger}) do
--- lib/hello_web/controllers/hello_html.ex
+++ lib/hello_web/controllers/hello_html.ex
@@ -3,4 +3,11 @@ defmodule HelloWeb.HelloHTML do
 
   embed_templates "hello_html/*"
 
+  attr :messenger, :string, required: true
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Hello World, from <b><%= @messenger %></b>!</h2>
+    """
+  end
 end
